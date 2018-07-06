68557bae55be84b25e4440bc4bdf8116e072eff2
coder-apex <coder.apex@gmail.com>
2018-07-06 22:47:32 +0530

    Array Methods snippet added

--- script.js
+++ script.js
@@ -44,7 +44,30 @@ for (let key in arr) {
   pr( arr[key] ); // Apple, Orange, Pear
 }
 
+/* Array Methods */
+/*  
+    arr.push(...items) – adds items to the end,
+    arr.pop() – extracts an item from the end,
+    arr.shift() – extracts an item from the beginning,
+    arr.unshift(...items) – adds items to the beginning.
+*/
+// arr.splice(str) can: add, remove and insert elements
+// Syntax: arr.splice(index[, deleteCount, elem1, ..., elemN])
+arr = ["I", "study", "JavaScript"];
+pr("\nSplicingInitial array is:");
+pr(arr);
 
+arr.splice(1, 1); // from index 1 remove 1 element
+pr("after splice(1,1) array is:");
+pr( arr ); // ["I", "JavaScript"]
+
+arr = ["I", "study", "JavaScript", "right", "now"];
+pr("Initial array is:");
+pr(arr);
+// remove 3 first elements and replace them with another
+arr.splice(0, 3, "Let's", "dance");
+pr("after splice(0, 3, \"Let's\", \"dance\") array is:");
+pr( arr ) // now ["Let's", "dance", "right", "now"]
 
 
 
